6745655eaa93b99e7a1c529854c8f16f2032f8a3
Nilam-Vaidya <nilamvaidya100@gmail.com>
2024-02-21 14:44:50 +0530

    styled header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import react, {component} from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <button>Add</button>
+      <div>
+        <span> Name </span>
+        <span> Phone </span>
+      </div>
+
+      <label id="name" htmlFor="name">Name : </label>
+      <input type="text" placeholder="your name" defaultValue="Nilam"></input>
+
+
     </div>
+    
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header= function(){
+  const headerStyle={padding: 20, textAlign:'center', background: 'black', color:'white', textTransform:'uppercase'};
+    return(
+        <div style={headerStyle}>
+        phone directory
+      </div>
+    )
+}
+
+export default Header;
